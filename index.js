4f258daa5a45a74e210e3e88c4cc2c492f5d2e4e
Nathan M <nathanminissale@gmail.com>
2018-09-30 08:49:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function takeANumber(initialList, newCustomer) {
-    var updatedList = initialList.push(newCustomer);
-    var postion = updatedList.indexOf(newCustomer) + 1;
+function takeANumber(customerList, newCustomer) {
+    var updatedList = customerList.push(newCustomer);
+    var postion = customerList.indexOf(newCustomer) + 1;
     var welcomeMessage = "Welcome, " + newCustomer + ". You are number " + postion + " in line.";
     return welcomeMessage;
 }
 
-function nowServing(currentLine) {
-
+function functionName() {
+  
 }
