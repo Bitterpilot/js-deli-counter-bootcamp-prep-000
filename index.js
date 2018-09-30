e580f3fc43e4ce9bd3951611d689db3fdba7c28a
Nathan M <nathanminissale@gmail.com>
2018-09-30 08:44:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(customerList, newCustomer) {
     var customerList2 = customerList.push(newCustomer);
-    var postion = customerList.indexOf();
+    var postion = customerList.indexOf() + 1;
     console.log(customerList);
     console.log(postion);
     var welcomeMessage = "Welcome, " + newCustomer + ". You are number " + postion + " in line.";
