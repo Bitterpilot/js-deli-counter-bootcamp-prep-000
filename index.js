b69a388cd8bccfc474a8be2f4615802a8d487d3b
Nathan M <nathanminissale@gmail.com>
2018-09-30 09:38:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(customerList) {
     customerList.forEach(
       function(item, index, array) {
         console.log(index + 1 + ".", item);
-        var st = index + 1 + ".", item;
+        var st = index + 1 + ". " + item;
         console.log(st);
       }
     );
